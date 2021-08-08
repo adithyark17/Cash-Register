b08659c5699d37488cba424536f1db81191f8984
Adithya Rajesh <52730950+adithyark17@users.noreply.github.com>
2021-08-08 14:18:28 +0530

    updated

--- app.js
+++
@@ -1,5 +0,0 @@
-const btn = document.querySelector("#btn")
-const billAmount = document.querySelector("#bill-amount")
-const cashGiven = document.querySelector("#cash-given")
-
-console.log(cashGiven.value);
