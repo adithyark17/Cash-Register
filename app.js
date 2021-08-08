a86d1972651d2e61a92e5d8e3b0e75380546fadd
Adithya Rajesh <52730950+adithyark17@users.noreply.github.com>
2021-08-08 17:17:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,4 @@ const btn = document.querySelector("#btn")
 const billAmount = document.querySelector("#bill-amount")
 const cashGiven = document.querySelector("#cash-given")
 
-billAmount.addEventListener("click", function checkbillandcash(){
-    console.log( billAmount.value > 0 ) {
-
-    } else 
-} )
+btn.addEventListener("click", () ==> console.log("clciked"))
