5b787d53f28503143f2a13d5502dcfa657e3a380
Adithya Rajesh <52730950+adithyark17@users.noreply.github.com>
2021-08-08 14:52:48 +0530

    updated

---
+++ app.js
@@ -0,0 +1,9 @@
+const btn = document.querySelector("#btn")
+const billAmount = document.querySelector("#bill-amount")
+const cashGiven = document.querySelector("#cash-given")
+
+billAmount.addEventListener("click", function checkbillandcash(){
+    console.log( billAmount.value > 0 ) {
+
+    } else 
+} )
