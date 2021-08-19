4aed24f42548d4eb85f17100bc795782c88812e4
Adithya Rajesh <52730950+adithyark17@users.noreply.github.com>
2021-08-19 10:24:42 +0530

    updated css

--- app.js
+++ app.js
@@ -1,5 +1,43 @@
 const btn = document.querySelector("#btn")
 const billAmount = document.querySelector("#bill-amount")
 const cashGiven = document.querySelector("#cash-given")
+const message = document.querySelector("#error-message")
+const numOfNotes = document.querySelectorAll(".no-of-notes")
 
-btn.addEventListener("click", () ==> console.log("clciked"))
+const availableNotes = [2000,500,100,50,20,10,1];
+
+btn.addEventListener("click", function validateBillAndCashAmount(){
+    hideMessage();
+    if (billAmount.value > 0 ) {
+        if (cashGiven.value >= billAmount.value){
+            const amountToBeReturned = cashGiven.value - billAmount.value;
+            calculateChange(amountToBeReturned);
+        }else{
+            showMessage("The amount should be atleast be equal to the bill amount");
+        }
+
+    } else {
+       showMessage("Invalid Bill Amount");
+    }
+});
+
+function calculateChange (amountToBeReturned) {
+    for ( let i = 0; i < availableNotes.length; i++){
+        const numOfNotes = Math.trunc(
+            amountToBeReturned / availableNotes[i]
+        );
+        amountToBeReturned %= availableNotes[i]; 
+        numOfNotes[i].innerText = numberOfNotes;
+    }
+
+}
+
+function hideMessage (){
+    message.style.display = "none";
+
+}
+
+function showMessage(msg){
+    message.style.display = "block";
+    message.innerText = msg;
+};
